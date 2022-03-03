dafa7c470b6250741151162f527094e1f6869b5c
Babi <babiferreirab15@gmail.com>
2022-03-03 23:23:50 +0100

    feito

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FruitsPage from "./store/modules/fruits/actions";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <FruitsPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/store/modules/fruits/reducer.js
@@ -0,0 +1,10 @@
+const initialState = ["Banana 🍌", "Maçã 🍎", "Morango 🍓"];
+
+const fruitsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default fruitsReducer;
